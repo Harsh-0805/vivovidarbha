80a279f4fd9bed6e344608b1b784f8f354de72bf
Sanchit Padwekar <86397529+soulfaced@users.noreply.github.com>
2024-10-19 16:39:54 +0530

    thodasa changes

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,7 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
-export const handler = NextAuth({
+const handler = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
