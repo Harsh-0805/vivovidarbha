d9140d3fc2f95fb58b47e079d73a7dfc697e77f0
Harsh-0805 <harshbadala0805@gmail.com>
2024-10-19 19:58:36 +0530

    working added

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,8 @@ import MustHaveProducts from "@/components/MustHave";
 import DiscoverSection from "@/components/Discover";
 import Chatbot from "@/components/Chatbot";
 import { Footer } from "@/components/Footer";
+import working from "@/public/assets/working.jpg";
+import Image from "next/image";
 
 const Home: React.FC = () => {
   return (
@@ -14,6 +16,16 @@ const Home: React.FC = () => {
       <Navbar />
       <HeroSection />
       {/* <ShopByCategory /> */}
+      <div className="relative w-full h-[500px] bg-gray-100 flex items-center justify-center overflow-hidden">
+        {/* Background Image */}
+        <Image
+          src={working} // Make sure to match the file path
+          alt="V40 & V40 Pro Banner"
+          layout="fill"
+          objectFit="contain"
+          className="absolute pt-10 rounded-3xl" // Ensures the image is behind the content
+        />
+      </div>
       <MustHaveProducts />
       <DiscoverSection />
       <WhyVivoNagpur />
