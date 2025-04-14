161edeca3c2f3b0d9f5cbdf185d62d4ba4978fca
Harsh-22f200474 <22f2000474@ds.study.iitm.ac.in>
2025-04-14 09:15:52 +0530

    fix

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -37,6 +37,12 @@ const slides: Slide[] = [
     objectFit: "contain",
     objectPosition: "center",
   },
+  {
+    imageMobile: "/assets/banner5.jpg",
+    imageDesktop: "/assets/banner5.jpg",
+    objectFit: "contain",
+    objectPosition: "center",
+  },
 ];
 
 export const HeroSection: React.FC = () => {
